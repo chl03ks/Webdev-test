828bcb2abdf1ff7debfcc445acc8f36c8f1cb87c
Dan <udevelopd@gmail.com>
2015-08-17 18:00:52 -0500

    adding the index template and bower configuration

--- mexicoder/views.py
+++ mexicoder/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+
+import requests
+import json
 
 
 def index(request):
-    return HttpResponse('Hello World!')
+    parsedData = []
+    if request.method == 'POST':
+        search = request.POST.get('search')
+        req = requests.get('https://api.github.com/users/' + search)
+        jsonList = []
+        jsonList.append(json.loads(req.content))
+        userData = {}
+        for data in jsonList:
+            userData['name'] = data['name']
+            userData['blog'] = data['blog']
+            userData['email'] = data['email']
+            userData['public_gists'] = data['public_gists']
+            userData['public_repos'] = data['public_repos']
+            userData['avatar_url'] = data['avatar_url']
+            userData['followers'] = data['followers']
+            userData['following'] = data['following']
+        parsedData.append(userData)
+    return render(request, 'mexicoder/index.html', {'data': parsedData})
