53f40a573356da238ddda4f0daa903540b345070
Dan <udevelopd@gmail.com>
2015-08-19 10:20:45 -0500

    Get tweets of the api and display it in the client

--- mexicoder/views.py
+++ mexicoder/views.py
@@ -1,25 +1,47 @@
+from __future__ import unicode_literals
 from django.shortcuts import render
+
+from twython import Twython
 
 import requests
 import json
 
+ckey = 'MsT8KiSsk3R7OhotmRCNKKChk'
+csecret = 'Wwm9raPcX1FFPktOdsXNqssFCCMAccL3qRhEvqrIxMYMhYWkfh'
+atoken = '2585921882-3BDMCj7L7egv2y6Av56syCY12dJsgoNXQP7Akia'
+asecret = 'ExtDJrjteOR40zKA3nFii0oHI1WR5XwMJZyWD16TEojKH'
+
+
+twitter = Twython(ckey, csecret, atoken, asecret)
+
+
+def search_twitter(search_term, geocode=None):
+    twitter = Twython(ckey, csecret)
+    if geocode:
+        result_search = twitter.search(q=search_term, geocode=geocode)
+    else:
+        result_search = twitter.search(q=search_term)
+    return result_search
+
+
+def utf8ify(list):
+    '''Encode a list of strings in utf8'''
+    return [item.encode('utf8') for item in list]
+
 
 def index(request):
-    parsedData = []
+    tweet_list = []
     if request.method == 'POST':
         search = request.POST.get('search')
         req = requests.get('https://api.github.com/users/' + search)
         jsonList = []
         jsonList.append(json.loads(req.content))
-        userData = {}
-        for data in jsonList:
-            userData['name'] = data['name']
-            userData['blog'] = data['blog']
-            userData['email'] = data['email']
-            userData['public_gists'] = data['public_gists']
-            userData['public_repos'] = data['public_repos']
-            userData['avatar_url'] = data['avatar_url']
-            userData['followers'] = data['followers']
-            userData['following'] = data['following']
-        parsedData.append(userData)
-    return render(request, 'mexicoder/index.html', {'data': parsedData})
+        data = search_twitter(search)
+        results = data['statuses']
+        for result in results:
+            tweet = dict()
+            tweet['user'] = result['user']['screen_name']
+            tweet['tweet'] = result['text']
+            tweet['img_url'] = result['user']['profile_image_url_https']
+            tweet_list.append(tweet)
+    return render(request, 'mexicoder/index.html', {'data': tweet_list})
